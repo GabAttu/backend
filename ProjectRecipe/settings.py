e44eb2b42828775bc7725bc7551b390bbd77a3dd
GabAttu <151763779+GabAttu@users.noreply.github.com>
2024-06-21 11:03:44 +0200

    Update settings.py

--- ProjectRecipe/settings.py
+++ ProjectRecipe/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-#3&vs9db_e$fc5)0h$3r+20y=lvsoel=^9qzerzaup^abjxe7d
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['web-production-dd25.up.railway.app']
 
 
 # Application definition
